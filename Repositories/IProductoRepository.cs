dbac2bc7aee729bad09f6ca1a689a67966a7add1
Yezid <ygm.mecatronico@gmail.com>
2025-08-06 01:26:36 -0500

    agregando IProductoRepository y ProductoRepository

---
+++ Repositories/IProductoRepository.cs
@@ -0,0 +1,13 @@
+﻿using PruebaQuala.Models;
+
+namespace PruebaQuala.Repositories
+{
+    public interface IProductoRepository
+    {
+        Producto GetById(int id);
+        List<Producto> GetAll();
+        Producto Add(Producto producto);
+        void Update(Producto producto);
+        void Delete(int id);
+    }
+}
---
+++ Repositories/ProductoRepository.cs
@@ -0,0 +1,38 @@
+﻿using PruebaQuala.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PruebaQuala.Repositories
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly IDbConnection _bd;
+
+        public ProductoRepository( IConfiguration configuration )
+        {
+            _bd = new SqlConnection(configuration.GetConnectionString("ConexionSQLLocalDB"));
+        }
+
+        public Producto GetById(int id) {
+
+            throw new NotImplementedException();
+        }
+        public List<Producto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public Producto Add(Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
